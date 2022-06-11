a4880ddc9ce478299fa6692171b516b19909eefb
cayan <filipecayan@gmail.com>
2022-06-11 20:22:40 -0300

    Testada

--- api/routes/club.js
+++ api/routes/club.js
@@ -108,6 +108,23 @@ async function routes (fastify, options) {
       
     },
   )
+
+  fastify.delete(
+    '/club/:id/delete_description',
+    async(request,reply)=>{
+      
+      const {id}=request.params
+      
+      fastify.knex('club')
+        .where('id',id)
+        .update({
+          description:null
+        })
+        .then((data)=>reply.send(data))
+
+
+    }
+  )
 }
     
 module.exports = routes
